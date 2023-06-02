d677a92764aeeb63751db4ffa803542585a0a5af
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-02 18:47:32 +0900

    #3 [build] - 스프링 프로젝트로 시작하기
    `start.spring.io` 를 사용하여 프로젝트초기화 하였습니다.
    `gitignore` 사이트를 사용하여 `.gitignore` 를 만들었습니다
    
    * 스프링 기본의존 추가
    * `.gitignore`

---
+++ haveit/src/main/java/com/mhc/haveit/HaveitApplication.java
@@ -0,0 +1,13 @@
+package com.mhc.haveit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaveitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaveitApplication.class, args);
+    }
+
+}
