6d6440f457791c8b5445bb595962ff1e1ddc3a81
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-04 18:10:43 +0900

    #7 [feat] - 도메인을 엔티티로 클래스로 변환
    1 도메인 == 1 테이블 매칭되도록 설계 했습니다.
    생성자와 `equeals()` , `hashcode()` 이
    도메인에 맞게 끔 설정하였습니다.
    
    * jpa audting 기능을 쓰기 위해 'jpaConfig' 추가
    * db 접근과 개발 편의를 위한 프로퍼티 적용

---
+++ haveit/src/main/java/com/mhc/haveit/domain/Article.java
@@ -0,0 +1,86 @@
+package com.mhc.haveit.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes ={
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Habit habit;
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;
+
+    @Setter @Column(nullable = false) private String title;
+    @Setter @Column(nullable = false) private String content;
+
+    @ToString.Exclude
+    @OrderBy("createdAt DESC ")
+    @OneToMany(mappedBy = "article")
+    private final Set<Comment> comments = new LinkedHashSet<>();
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String modifiedBy;
+
+    protected Article() {}
+
+    private Article (Habit habit, UserAccount userAccount, String title, String content) {
+        this.habit = habit;
+        this.userAccount = userAccount;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Article of (Habit habit, UserAccount userAccount, String title, String content) {
+       return new Article(habit, userAccount, title, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ haveit/src/main/java/com/mhc/haveit/domain/HabitRegistration.java
@@ -0,0 +1,74 @@
+package com.mhc.haveit.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class HabitRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Habit habit;
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String createdBy;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @LastModifiedBy
+    @Column(nullable = false, updatable = false, length = 100)
+    private String modifiedBy;
+
+    private HabitRegistration(Habit habit, UserAccount userAccount) {
+        this.habit = habit;
+        this.userAccount = userAccount;
+    }
+
+    protected HabitRegistration () {}
+
+    public static HabitRegistration of (Habit habit, UserAccount userAccount) {
+        return new HabitRegistration(habit, userAccount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof HabitRegistration habitRegistration)) return false;
+        return id != null && id.equals(habitRegistration.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
