de17f3120d68b25d317f98fb7567d35881c4b944
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-08 22:06:01 +0900

    #15 [feat] - data rest 설정
    습관, 게시글, 댓글의 json api 를 자동으로 
    restful 하게 만들게끔 설정

--- haveit/src/main/java/com/mhc/haveit/repository/CommentRepository.java
+++ haveit/src/main/java/com/mhc/haveit/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.mhc.haveit.repository;
 
 import com.mhc.haveit.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
