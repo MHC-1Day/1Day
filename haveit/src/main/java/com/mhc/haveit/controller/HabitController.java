870bcf84a9dd56a070fbbac656c4a954a9c7054f
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-09 18:29:49 +0900

    #18 [test] - HabitController 테스트 정의
    HabitController 의 기본 테스트를 정의 하였습니다.
    
    기존 테스트를 실패한 채 올리면 `gradle build` 가 실패하여
    실패 테스트는 테스트 제외 처리하도록 하였습니다.
    이에 기능 구현 중인 유닛 테스트를 모두 `@Disabled` 처리 하였습니다.

---
+++ haveit/src/main/java/com/mhc/haveit/controller/HabitController.java
@@ -0,0 +1,9 @@
+package com.mhc.haveit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/habits")
+@Controller
+public class HabitController {
+}
---
+++ haveit/src/test/java/com/mhc/haveit/controller/HabitControllerTest.java
@@ -0,0 +1,46 @@
+package com.mhc.haveit.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(HabitController.class)
+class HabitControllerTest {
+
+    private final MockMvc mvc;
+
+    public HabitControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("개발중")
+    @DisplayName("[view][GET] 습관 리스트 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingHabitsView_thenReturnsHabitsView() throws Exception {
+        mvc.perform(get("/habits"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("habits/index"))
+                .andExpect(model().attributeExists("habits"));
+    }
+
+    @Disabled("개발중")
+    @DisplayName("[view][GET] 습관 상세 페이지 - 정상 호출")
+    @Test
+    void givenNothing_whenRequestingHabitView_thenReturnsHabitView() throws Exception {
+        mvc.perform(get("/habits/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("habits/detail"))
+                .andExpect(model().attributeExists("habit"));
+    }
+
+}
