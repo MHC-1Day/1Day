60b9bbbd50656dcd2f8236fd9bc0da4a72297a40
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-14 21:04:42 +0900

    #18 [feat] - 습관목록 View 추가
    습관 목록을 출력하는 간단한 view 를 만들었습니다.
    view 를 테스트 하기위해,
    Controller 는 비어있는 List 를 반환합니다.

--- haveit/src/main/java/com/mhc/haveit/controller/HabitController.java
+++ haveit/src/main/java/com/mhc/haveit/controller/HabitController.java
@@ -1,9 +1,18 @@
 package com.mhc.haveit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/habits")
 @Controller
 public class HabitController {
+    @GetMapping
+    public String habits(ModelMap map){
+        map.addAttribute("habits", List.of());  // TODO : service 구현 시 바꿔야 함
+        return "habits/index";
+    }
 }
--- haveit/src/test/java/com/mhc/haveit/controller/HabitControllerTest.java
+++ haveit/src/test/java/com/mhc/haveit/controller/HabitControllerTest.java
@@ -1,10 +1,12 @@
 package com.mhc.haveit.controller;
 
+import com.mhc.haveit.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 게시글")
 @WebMvcTest(HabitController.class)
+@Import(SecurityConfig.class)
 class HabitControllerTest {
 
     private final MockMvc mvc;
@@ -21,13 +24,12 @@ class HabitControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("개발중")
     @DisplayName("[view][GET] 습관 리스트 페이지 - 정상 호출")
     @Test
     void givenNothing_whenRequestingHabitsView_thenReturnsHabitsView() throws Exception {
         mvc.perform(get("/habits"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("habits/index"))
                 .andExpect(model().attributeExists("habits"));
     }
@@ -38,7 +40,7 @@ class HabitControllerTest {
     void givenNothing_whenRequestingHabitView_thenReturnsHabitView() throws Exception {
         mvc.perform(get("/habits/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("habits/detail"))
                 .andExpect(model().attributeExists("habit"));
     }
